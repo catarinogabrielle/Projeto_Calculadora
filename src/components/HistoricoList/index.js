d3217467b55db1f7044a824e20e9bdba4027014c
Gabrielle De Castro Santos Catarino <54675970+catarinogabrielle@users.noreply.github.com>
2021-04-20 18:50:36 -0300

    5

--- src/components/HistoricoList/index.js
+++ src/components/HistoricoList/index.js
@@ -1,35 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+
+import { Container, IdadeText } from './styles';
 
 export default function HistoricoList({ data }) {
     return (
-        <View style={styles.container}>
-            <Text style={styles.idade}>{data.idade}</Text>
-        </View>
+        <Container>
+            <IdadeText>{data.idade}</IdadeText>
+        </Container>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 5,
-        marginLeft: 12,
-        backgroundColor: '#f1f1f1',
-        width: '35%',
-        marginBottom: 15,
-        alignItems: 'center',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.23,
-        shadowRadius: 2.62,
-
-        elevation: 4,
-    },
-
-    idade: {
-        color: '#000',
-        fontSize: 30,
-    }
-});
---
+++ src/components/HistoricoList/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+padding: 5px;
+margin-left: 12px;
+background: #f1f1f1;
+width: 35%;
+margin-bottom: 15px;
+align-items: center;
+`;
+
+export const IdadeText = styled.Text`
+color: ${props => props.idade === 28 ? '#2BB1DB' : '#E48A1F'};
+font-size: 30px;
+`;
